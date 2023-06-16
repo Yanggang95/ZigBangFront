f04cc129592ce4be375d50fa28fa531f90c5f3df
aypd02150@gmail.com <aypd02150@gmail.com>
2023-06-17 00:09:03 +0900

    Initial commit

---
+++ src/Hello/Hello.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p>시발</p>
+        </div>
+    )
+}
+
+export default Header
+
